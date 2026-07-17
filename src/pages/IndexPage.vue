5ec2f14543d6c921d0f038b0b21a983d6bacb377
nontiwat1307 <nontiwat1307@gmail.com>
2026-07-17 14:58:40 +0700

    Thai-lang add

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,54 +1,83 @@
+# เปลี่ยนข้อความในฟอร์เป็นภาษาไทย
 <template>
-<div class="q-pa-md" style="max-width: 400px">
-    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-        <q-input filled v-model="name" label="Your name*" hint="Name and surname" lazy-rules :rules="[val => (val && val.length > 0) || 'Please type something']" />
+  <div class="q-pa-md" style="max-width: 400px">
 
-        <q-input filled type="number" v-model.number="age" label="Your age *" lazy-rules :rules="[
-        val => (val !== null && val !== '') || 'Please type your age',
-        val => (val > 0 && val < 100) || 'Please type a real age'
-      ]" />
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="name"
+        label="ชื่อ-สกุล *"
+        hint="ชื่อและนามสกุล"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'กรุณาพิมพ์ชื่อ']"
+      />
 
-        <q-toggle v-model="accept" label="I accept the license and terms" />
+      <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="อายุ *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'กรุณาใส่อายุ',
+          val => val > 0 && val < 100 || 'กรุณาใส่อายุจริง'
+        ]"
+      />
 
-        <div>
-            <q-btn label="Submit" type="submit" color="primary" />
-            <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-        </div>
+      <q-toggle v-model="accept" label="ยอมรับ" />
+
+      <div>
+        <q-btn label="ยอมรับ" type="submit" color="primary"/>
+        <q-btn label="ยกเลิก" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
     </q-form>
-</div>
+
+  </div>
 </template>
 
 <script>
 import { useQuasar } from 'quasar'
 import { ref } from 'vue'
 
-const $q = useQuasar()
+export default {
+  setup () {
+    const $q = useQuasar()
 
-const name = ref(null)
-const age = ref(null)
-const accept = ref(false)
-
-function onSubmit() {
-  if (accept.value !== true) {
-    $q.notify({
-      color: 'red-5',
-      textColor: 'white',
-      icon: 'warning',
-      message: 'You need to accept the license and terms first'
-    })
-  } else {
-    $q.notify({
-      color: 'green-4',
-      textColor: 'white',
-      icon: 'cloud_done',
-      message: 'Submitted'
-    })
+    const name = ref(null)
+    const age = ref(null)
+    const accept = ref(false)
+    return {
+      name,
+      age,
+      accept,
+      onSubmit () {
+        if (accept.value !== true) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'คุณจำเป็นต้องยอมรับ'
+          })
+        }
+        else {
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'ข้อมูลได้รับการยืนยัน'
+          })
+        }
+      },
+      onReset () {
+        name.value = null
+        age.value = null
+        accept.value = false
+      }
+    }
   }
-}
-
-function onReset() {
-  name.value = null
-  age.value = null
-  accept.value = false
 }
 </script>
